91ad70141564d35101db4acc55ca499757254467
Aaron Tomb <atomb@galois.com>
2021-09-01 13:46:52 -0700

    Set host and build flags when compiling Yices

--- .github/ci.sh
+++ .github/ci.sh
@@ -38,7 +38,7 @@ build_yices() {
   pushd yices2-Yices-$YICES_VERSION
   autoconf
   if $IS_WIN; then
-    ./configure --host=x86_64-w64-mingw32
+    ./configure --host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32
   else
     ./configure
   fi
