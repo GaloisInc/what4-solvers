b70fe17218b9f28802e78772b8100287db9c36e3
Aaron Tomb <atomb@galois.com>
2021-09-08 10:43:49 -0700

    Include CVC4 binary for Windows

--- .github/ci.sh
+++ .github/ci.sh
@@ -27,6 +27,7 @@ build_cvc4() {
   if $IS_WIN ; then
     echo "Downloading pre-built CVC4 binary for Windows"
     curl -o cvc4$EXT -sL "https://github.com/CVC4/CVC4/releases/download/1.8/cvc4-1.8-win64-opt.exe"
+    cp cvc4$EXT $BIN
   else
     ./contrib/get-antlr-3.4
     ./configure.sh --static --no-static-binary production
