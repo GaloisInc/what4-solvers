a64e0aa5aafeef5e12ecb2bd1e5e9df243f34cc2
Michal Podhradsky <mpodhradsky@galois.com>
2026-01-21 09:15:55 -0800

    Simplify solver testing

--- .github/ci.sh
+++ .github/ci.sh
@@ -315,27 +315,38 @@ cleanup_bins() {
 }
 
 # Test a solver by running it on the test problem
-# Usage: test_solver <solver_name>
+# Usage: test_solver <solver_name> [use_system_path]
+# Arguments:
+#   solver_name      Name of the solver to test
+#   use_system_path  If "true", use solvers from system PATH instead of local ./bin directory
 test_solver() {
   SOLVER="$1"
+  USE_SYSTEM_PATH="${2:-false}"
   echo "Testing $SOLVER with $PROBLEM..."
 
-  cd "$BIN"
+  if $USE_SYSTEM_PATH; then
+    # Use solvers from system PATH
+    SOLVER_PREFIX=""
+  else
+    # Use solvers from local bin directory
+    cd "$BIN"
+    SOLVER_PREFIX="./"
+  fi
 
   # Run the solver and capture output
   case "$SOLVER" in
     abc)
-      RESULT=$(deps abc$EXT && ./abc$EXT -S "%blast; &sweep -C 5000; &syn4; &cec -m -s" < "$PROBLEM")
+      RESULT=$(deps ${SOLVER_PREFIX}abc$EXT && ${SOLVER_PREFIX}abc$EXT -S "%blast; &sweep -C 5000; &syn4; &cec -m -s" < "$PROBLEM")
       ;;
     boolector)
-      RESULT=$(./boolector$EXT --version && deps boolector$EXT && ./boolector$EXT "$PROBLEM" --no-exit-codes)
+      RESULT=$(${SOLVER_PREFIX}boolector$EXT --version && deps ${SOLVER_PREFIX}boolector$EXT && ${SOLVER_PREFIX}boolector$EXT "$PROBLEM" --no-exit-codes)
       ;;
     yices)
-      RESULT=$(./yices-smt2$EXT --version && deps yices-smt2$EXT && ./yices-smt2$EXT "$PROBLEM")
+      RESULT=$(${SOLVER_PREFIX}yices-smt2$EXT --version && deps ${SOLVER_PREFIX}yices-smt2$EXT && ${SOLVER_PREFIX}yices-smt2$EXT "$PROBLEM")
       ;;
     *)
       # Most solvers use the same invocation pattern
-      RESULT=$(./$SOLVER$EXT --version && deps $SOLVER$EXT && ./$SOLVER$EXT "$PROBLEM")
+      RESULT=$(${SOLVER_PREFIX}$SOLVER$EXT --version && deps ${SOLVER_PREFIX}$SOLVER$EXT && ${SOLVER_PREFIX}$SOLVER$EXT "$PROBLEM")
       ;;
   esac
 
@@ -347,6 +358,15 @@ test_solver() {
     echo "$RESULT"
     return 1
   fi
+}
+
+# Test all solvers, assume they are on the PATH
+test_all_solvers() {
+  for solver in abc bitwuzla boolector cvc4 cvc5 yices z3-4.8.8 z3-4.8.14
+  do
+    which $solver
+    test_solver $solver true
+  done
 }
 
 # GitHub Actions' Ubuntu runners have somewhat unusual naming conventions. For
